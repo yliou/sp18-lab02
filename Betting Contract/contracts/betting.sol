ba0011f0883dd100b1130f9c3472bbb01b130819
yliou <yliou@berkeley.edu>
2018-02-23 17:55:28 -0800

    first submission of betting contract, many parts open to interpretation

--- Betting Contract/contracts/betting.sol
+++ Betting Contract/contracts/betting.sol
@@ -4,6 +4,8 @@ pragma solidity 0.4.19;
 contract Betting {
     /* Constructor function, where owner and outcomes are set */
     function Betting(uint[] _outcomes) public {
+        owner = tx.origin;
+        uint[] memory outcomes = _outcomes;
     }
 
     /* Fallback function */
@@ -16,6 +18,7 @@ contract Betting {
     address public gamblerA;
     address public gamblerB;
     address public oracle;
+    uint public count = 0;
 
     /* Structs are custom data structures with self-defined parameters */
     struct Bet {
@@ -42,29 +45,64 @@ contract Betting {
 
     /* Owner chooses their trusted Oracle */
     function chooseOracle(address _oracle) public ownerOnly() returns (address) {
+        return _oracle;
     }
 
     /* Gamblers place their bets, preferably after calling checkOutcomes */
-    function makeBet(uint _outcome) public payable returns (bool) {
+    function makeBet(uint _outcome, uint _amount) public payable returns (bool) {
+        if (count == 2) {
+            return false;
+        }
+        if (bets[tx.origin].initialized == false) {
+            bets[tx.origin].outcome = _outcome;
+            bets[tx.origin].initialized = true;
+            bets[tx.origin].amount = _amount;
+            count = count + 1;
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     /* The oracle chooses which outcome wins */
     function makeDecision(uint _outcome) public oracleOnly() outcomeExists(_outcome) {
+        if (bets[gamblerA].outcome == _outcome && bets[gamblerB].outcome == _outcome) {
+            winnings[gamblerA] = bets[gamblerA].amount;
+            winnings[gamblerB] = bets[gamblerB].amount;
+        }
+        if (bets[gamblerA].outcome != _outcome && bets[gamblerB].outcome == _outcome) {
+            winnings[gamblerA] = 0;
+            winnings[gamblerB] = bets[gamblerA].amount + bets[gamblerB].amount;
+        }
+        if (bets[gamblerA].outcome == _outcome && bets[gamblerB].outcome != _outcome) {
+            winnings[gamblerA] = bets[gamblerA].amount + bets[gamblerB].amount;
+            winnings[gamblerB] = 0;
+        }
+        else {
+            winnings[gamblerA] = 0;
+            winnings[gamblerB] = 0;
+            winnings[oracle] = bets[gamblerA].amount + bets[gamblerB].amount;
+        }
     }
 
     /* Allow anyone to withdraw their winnings safely (if they have enough) */
     function withdraw(uint withdrawAmount) public returns (uint) {
-    }
-    
-    /* Allow anyone to check the outcomes they can bet on */
-    function checkOutcomes(uint outcome) public view returns (uint) {
-    }
-    
-    /* Allow anyone to check if they won any bets */
-    function checkWinnings() public view returns(uint) {
+        bool ret = tx.origin.send(winnings[tx.origin]);
+        return winnings[tx.origin];
     }
 
-    /* Call delete() to reset certain state variables. Which ones? That's upto you to decide */
+    /* Allow anyone to check the outcomes they can bet on */
+    function checkOutcomes(uint outcome) public view returns (uint) {
+        return outcomes[outcome];
+    }
+
+    /* Allow anyone to check if they won any bets */
+    function checkWinnings() public view returns(uint) {
+        return winnings[tx.origin];
+    }
+
+    /* Call delete() to reset certain state variables. Which ones? That's up to you to decide */
     function contractReset() public ownerOnly() {
     }
 }
